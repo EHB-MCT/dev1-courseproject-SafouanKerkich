481b8d332c505fff125b4c4f0047b669b871a996
FIRST_NAME LAST_NAME <safouan.kerkich@student.ehb.be>
2024-11-13 22:55:06 +0100

    canvas setup

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-"use strict";
+// setup canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+function setupCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    drawStarrySky();
+}
+
+
+
+setupCanvas(); 
 
