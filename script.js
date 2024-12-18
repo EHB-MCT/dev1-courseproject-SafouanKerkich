fefefb9a726f3c85c96e7ad0895af7da2998265c
Kerkich Safouan <safouan.kerkich@student.ehb.be>
2024-12-18 22:25:32 +0100

    genereren van 500 verschillende sterren

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 "use strict";
 
-// Canvas opzetten
+// Canvas setup
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-console.log("Canvas:", canvas);
-console.log("Context:", context);
+// Variabelen
+const stars = []; 
+const numberOfStars = 500; 
 
-
+// Canvasgrootte instellen
 function resizeCanvas() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    drawBackground();
+    initializeStars(); 
+    drawStars(); 
 }
 
-function drawBackground() {
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+// Sterren 
+function initializeStars() {
+    stars.length = 0; 
+    for (let i = 0; i < numberOfStars; i++) {
+        stars.push({
+            x: Math.random() * canvas.width, 
+            y: Math.random() * canvas.height, 
+            size: Math.random() * 2 + 1, 
+        });
+    }
 }
 
+// Sterren tekenen
+function drawStars() {
+    context.fillStyle = "black"; 
+    context.fillRect(0, 0, canvas.width, canvas.height); 
+
+    context.fillStyle = "white"; 
+    for (const star of stars) {
+        context.beginPath();
+        context.arc(star.x, star.y, star.size, 0, Math.PI * 2); 
+        context.fill();
+    }
+}
+
+// Event listener voor resize
 window.addEventListener("resize", resizeCanvas);
-resizeCanvas();
+resizeCanvas(); 
